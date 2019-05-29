2c38d106c4095c692e8a67e86ed4ced29d139649
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-28 23:15:01 -0300

    ENH: update description

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Projeto disponível em
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/marco-souza/react-native-vs-pwa-demo/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          github.com/marco-souza/react-native-vs-pwa-demo/
         </a>
       </header>
     </div>
